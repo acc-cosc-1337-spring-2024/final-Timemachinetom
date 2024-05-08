f4ef2698e06e44d265a1c9497234649623631155
Timemachinetom <108629469+Timemachinetom@users.noreply.github.com>
2024-05-08 02:39:15 +0000

    Question a

--- src/question_a/main.py
+++ src/question_a/main.py
@@ -1,1 +1,18 @@
 #add import
+import question_a
+nums = []
+while len(nums)!= 5:
+    num = input ('Enter a number: ')
+    num = int(num)
+    nums.append(num)
+if len(nums) == 5:
+    low = question_a.get_lowest(nums)
+    print ("Lowest number:", low)
+    high = question_a.get_highest(nums)
+    print ("Highest number:", high)
+    sum = question_a.get_sum(nums)
+    print ("Sum of numbers:", sum)
+    avg = question_a.get_average(nums)
+    print ("Average of numbers:", avg)
+
+
--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,20 @@
 #write functions here, don't add input('') statements here!
 def test_config():
     return True
+def get_lowest(nums):
+    low = min(nums)
+    return low
+
+def get_highest(nums):
+    high = max(nums)
+    return high
+
+def get_sum(nums):
+    total = sum(nums)
+    return total
+
+def get_average(nums):
+    total= sum(nums)
+    size = len(nums)
+    avg = total/size
+    return avg
