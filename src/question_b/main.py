e18823808a5936abd3dcdcc65ff37e22e856263e
Timemachinetom <108629469+Timemachinetom@users.noreply.github.com>
2024-05-09 02:31:56 +0000

    Question B

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,52 @@
 #add import
+import question_b
+from question_b import Stock
+
+option = 0
+quit = False
+
+while quit != True:
+    option = input ("Question B Menu\n1-Display Stock Purchase History\n2-Exit\n")
+    option = int(option)
+
+    if option == 1:
+        question_b.stock_purchase_history()
+        exit = input ("Do you want to exit?\n1 - Yes\n2 - No\n")
+        exit = int(exit)
+        if exit == 1:
+            exit = 0
+            option = 0
+            sure = input ("Are you sure you want to exit?\n1 - Yes\n2 - No\n")
+            sure = int(sure)
+            if sure == 1:
+                exit = 0
+                option = 0
+                print("Goodbye")
+                quit = True
+            elif sure == 2:
+                sure = 0
+                exit = 0
+                option = 0
+            else:
+                sure = 0
+                exit = 0
+                option = 0
+        elif exit == 2:
+            exit = 0
+            option = 0 
+        else:
+            exit = 0
+            option = 0
+    if option == 2:
+        exit = input ("Are you sure you want to exit?\n1 - Yes\n2 - No\n")
+        exit = int(exit)
+        if exit == 1:
+            exit = 0
+            option = 0
+            print("Goodbye")
+            quit = True
+        elif exit == 2:
+            exit = 0
+            option = 0
+    else:
+        option = 0
--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,27 @@
 #write functions here, don't add input('') statements here!
+
+class Stock:
+    def __init__(self, symbol, company_name):
+        self._symbol = symbol
+        self._company_name = company_name
+    
+    def return_symbol (self):
+        return (self._symbol)
+    
+    def return_company_name(self):
+        return (self._company_name)
+
+def stock_purchase_history ():
+    stock_history = {}
+    stock_1 = Stock("AAPL", "Apple Inc")
+    stock_history [stock_1.return_symbol()] = stock_1.return_company_name()
+    stock_2 = Stock("CAT", "Caterpillar")
+    stock_history [stock_2.return_symbol()] = stock_2.return_company_name()
+    stock_3 = Stock("EK", "Eastman Kodak")
+    stock_history [stock_3.return_symbol()] = stock_3.return_company_name()
+    stock_4 = Stock("GOOG", "Google")
+    stock_history [stock_4.return_symbol()] = stock_4.return_company_name()
+    stock_5 = Stock("MSFT", "Microsoft")
+    stock_history [stock_5.return_symbol()] = stock_5.return_company_name()
+    for key, value in stock_history.items():
+        print (key, "\t", value, "\n")
