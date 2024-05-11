c83e9ca6bb8016aa5251a58c5783cb1368f09fc3
Timemachinetom <108629469+Timemachinetom@users.noreply.github.com>
2024-05-11 16:59:40 +0000

    Question D

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,31 @@
-#add import
+import question_d
+from question_d import create_multiplication_table
+
+exit = 0
+quit = False
+
+while quit != True:
+    table = question_d.create_multiplication_table ()
+    question_d.display_multiplication_table (table)
+    exit = input ("\n\nWould you like to see the multiplication table again?\n1 - Yes\n2 - Quit\n")
+    exit = int(exit)
+    if exit == 2:
+        sure = input ("Are you sure you want to exit?\n1 - Yes\n2 - No\n")
+        sure = int(sure)
+        if sure == 1:
+            exit = 0
+            print("Goodbye")
+            quit = True
+        elif sure == 2:
+            sure = 0
+            exit = 0
+        else:
+            sure = 0
+            exit = 0
+
+
+
+
+
+
+
--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,21 @@
-#write functions here, don't add input('') statements here!
+
+def create_multiplication_table ():
+    table = []
+    for column in range (1,6):
+        for row in range (1,11):
+            table.append (row*column)
+    return (table)
+
+def display_multiplication_table (table):
+    go = True
+    start = 0
+    end = 10
+    while go == True:
+        row = []
+        for i in range (start, end):
+            row.append (table[i])
+        print (*row)
+        start += 10
+        end += 10
+        if start == 50:
+            go = False
